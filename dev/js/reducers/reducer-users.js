521096a0c6a148b453777480789ffadfa65cfcca
Natalia Kukula <nataliakukula@Natalias-MacBook-Pro.local>
2019-06-28 10:55:06 -0500

    added reducers

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function () {
+  return [
+    {
+      id: 1,
+      first: "Bucky",
+      last: "Roberts",
+      age: 71,
+      description: "Bucky is a React developer and YouTuber",
+      thumbnail: "https://randomuser.me/api/portraits/men/44.jpg"
+    },
+    {
+      id: 1,
+      first: "Jen",
+      last: "Kale",
+      age: 13,
+      description: "Jen likes kale",
+      thumbnail: "https://randomuser.me/api/portraits/women/90.jpg"
+    },
+    {
+      id: 1,
+      first: "Mark",
+      last: "Twain",
+      age: 99,
+      description: "Mark feels overwhelmed by the current times",
+      thumbnail: "https://randomuser.me/api/portraits/men/13.jpg"
+    },
+  ]
+}
